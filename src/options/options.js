3258ddc9008186309d6dce7af02bb43711b6a747
KartoffelChips <jan@strassburger.org>
2023-08-06 23:09:20 +0200

    started internationalization, added help sections

--- src/options/options.js
+++ src/options/options.js
@@ -32,3 +32,23 @@ testVideoperms()
 requestVideoPermBtn.onclick = () => {
     testVideoperms();
 };
+
+document.querySelectorAll('[data-locale]').forEach(elem => {
+    elem.innerText = chrome.i18n.getMessage(elem.dataset.locale)
+});
+
+/*--- Collapsible ---*/
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+        this.classList.toggle("active");
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+            content.style.maxHeight = null;
+        } else {
+            content.style.maxHeight = content.scrollHeight + "px";
+        }
+    });
+}
